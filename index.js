a0a06809ce62e296d66c7462f95595c595b548b8
Jabarre <jjarrett.prsvr@gmail.com>
2022-11-10 19:20:41 -0600

    made some changes again to my .js file

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 //let sumEl  = document.getElementById("sum-el")its the same thing as line 11 and its also the same concept as line 9
 let sumEl  = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() *13 ) + 1
+    return randomCard
+}
+console.log(randomCard())
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
 function renderGame(){
     cardsEl.textContent = "Cards: "
     for(let i= 0; i < cards.length; i++) {
@@ -22,10 +37,12 @@ function renderGame(){
         message = "Wohoo! You've got Blackjack!"
     
     }else {
+        isAlive = false
         message = "you're out of the game"};
         console.log("message")
-        messageEl.textContent = message;
+        
     }
+        messageEl.textContent = message;
     function newCard() {
         let card = getRandomCard
         sum += card
